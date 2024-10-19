bb64ba3ba5452a1bac3295d4bc4c8a0546fb32ab
amoghsp2000 <52058736+amoghsp2000@users.noreply.github.com>
2024-10-19 17:15:14 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect("");
+        const connect = await mongoose.connect("mongodb+srv://habitspark01:1BMv51tTs2yYBQXK@team.ymo8m.mongodb.net/habits-backend?retryWrites=true&w=majority&appName=Team");
         console.log("Database connected: ", connect.connection.host, connect.connection.name);
     } catch (err) {
         console.log(err);
